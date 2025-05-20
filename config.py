9d55f643c1c5bcf1d1494ab79426cb78bb99cec5
ᴛɪᴍᴇ ɪs ᴍᴏɴᴇʏ <codexyraa@gmail.com>
2025-05-20 15:53:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ class Config:
         'Join first, then press the Try Again button.'
     )
 
-    API_ID = int(os.environ.get('API_ID', 2040))
-    API_HASH = os.environ.get('API_HASH', 'b18441a1ff607e10a989891a5462e627')
-    OWNER_ID = int(os.environ.get('OWNER_ID', 487936750))
-    MONGO_URL = os.environ.get('MONGO_URL', 'mongodb://root:passwd@mongo')
+    API_ID = int(os.environ.get('API_ID', 20897598))
+    API_HASH = os.environ.get('API_HASH', '93acdbdc4b23c0398bd2042d5ee3865e')
+    OWNER_ID = int(os.environ.get('OWNER_ID', 1927018403))
+    MONGO_URL = os.environ.get('MONGO_URL', 'mongodb+srv://wtfbruh:KontolXD#123@fsub.brzgete.mongodb.net/?retryWrites=true&w=majority&appName=fsub')
 
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
-    DATABASE_ID = int(os.environ.get('DATABASE_ID', ''))
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '7625651439:AAHtJj_3CoSAhNi6CkH4lZgazEYLcn74ugE')
+    DATABASE_ID = int(os.environ.get('DATABASE_ID', '-1002532922539'))
 
     BOT_ID = BOT_TOKEN.split(':', 1)[0]
 
